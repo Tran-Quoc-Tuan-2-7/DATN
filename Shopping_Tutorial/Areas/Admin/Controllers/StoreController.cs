7153522f97854c963439b217cc77412135879757
tuan.tran6 <tuan.tran6@sotatek.com>
2025-06-28 09:57:55 +0700

    update

--- Shopping_Tutorial/Areas/Admin/Controllers/StoreController.cs
+++ Shopping_Tutorial/Areas/Admin/Controllers/StoreController.cs
@@ -24,9 +24,84 @@ public class StoreController : Controller
         return View(store);
     }
 
-    public async Task<IActionResult> Edit()
+    public async Task<IActionResult> Edit(int Id)
     {
-        StoreModel store = await _dataContext.Stores.FirstOrDefaultAsync();
+        var store = await _dataContext.Stores.FindAsync(Id);
+        if (store == null)
+        {
+            return NotFound();
+        }
         return View(store);
+    }
+
+
+    public async Task<IActionResult> Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(StoreModel Store)
+    {
+        if (ModelState.IsValid)
+        {
+            _dataContext.Add(Store);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Thêm cửa hàng thành công";
+            return RedirectToAction("Index");
+        }
+
+        else
+        {
+            TempData["error"] = "Có một vài thứ đang bị lỗi";
+            List<string> errors = new List<string>();
+            foreach (var value in ModelState.Values)
+            {
+                foreach (var error in value.Errors)
+                {
+                    errors.Add(error.ErrorMessage);
+                }
+            }
+            string errorMessage = string.Join("\n", errors);
+            return BadRequest(errorMessage);
+        }
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(StoreModel Store)
+    {
+        if (ModelState.IsValid)
+        {
+            _dataContext.Update(Store);
+            await _dataContext.SaveChangesAsync();
+            TempData["success"] = "Sửa cửa hàng thành công";
+            return RedirectToAction("Index");
+        }
+
+        else
+        {
+            TempData["error"] = "Có một vài thứ đang bị lỗi";
+            List<string> errors = new List<string>();
+            foreach (var value in ModelState.Values)
+            {
+                foreach (var error in value.Errors)
+                {
+                    errors.Add(error.ErrorMessage);
+                }
+            }
+            string errorMessage = string.Join("\n", errors);
+            return BadRequest(errorMessage);
+        }
+    }
+
+    public async Task<IActionResult> Delete(int Id)
+    {
+        StoreModel Store = await _dataContext.Stores.FindAsync(Id);
+        _dataContext.Stores.Remove(Store);
+        await _dataContext.SaveChangesAsync();
+        TempData["success"] = "Cửa hàng đã xóa thành công";
+        return RedirectToAction("Index");
     }
 }
